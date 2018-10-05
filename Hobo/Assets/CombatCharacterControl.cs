782b90957be8497a7ac9b8d422a37532bc60165c
SamuelQZQ <928221136@qq.com>
2018-10-05 15:51:40 +0800

    Combat character control

--- Hobo/Assets/CombatCharacterControl.cs
+++ Hobo/Assets/CombatCharacterControl.cs
@@ -12,7 +12,24 @@ public class CombatCharacterControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        // move
         rigidbody.AddForce(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
 
+        // rotation
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 thisPos = this.transform.position;
+            float targetAngle = Mathf.Atan2(mousePos.y - thisPos.y, mousePos.x - thisPos.x);
+            float origAngle = this.transform.eulerAngles.y;
+            Debug.Log(Mathf.Rad2Deg * targetAngle);
+            Debug.Log(this.transform.eulerAngles.y);
+            this.transform.Rotate(0, 0, Mathf.Rad2Deg * targetAngle - this.transform.eulerAngles.z);
+
+            //Vector3 relativePos = mousePos - transform.position;
+            //Quaternion rotation = Quaternion.LookRotation(relativePos);
+            //transform.rotation = rotation;
+
+        }
 	}
 }
