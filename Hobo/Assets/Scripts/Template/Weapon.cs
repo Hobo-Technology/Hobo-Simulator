d580a560e99a2526faaf3c71027d553a894facdd
SamuelQZQ <928221136@qq.com>
2018-10-05 23:40:32 +0800

    Weapon example

---
+++ Hobo/Assets/Scripts/Template/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = ("RPG/Weapon"))]
+public class Weapon : ScriptableObject
+{
+    [SerializeField] Color color;
+    [Range(1,2)] public float baoji;
+    [SerializeField] int attack;
+    [SerializeField] GameObject weaponPrefab;
+    [SerializeField] AnimationClip attackAnimation;
+
+    public GameObject GetWeaponPrefab()
+    {
+        return weaponPrefab;
+    }
+
+    public AnimationClip GetAnimation()
+    {
+        return attackAnimation;
+    }
+}
