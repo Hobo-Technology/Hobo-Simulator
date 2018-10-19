4559a8d055d6aee0fb587a58880a8c227e7ff584
snowysmile <snowy_smile@qq.com>
2018-10-19 15:26:13 +0800

    back_to_work

--- Hobo/Assets/Scripts/Combat/CombatCharacterControl.cs
+++ Hobo/Assets/Scripts/Combat/CombatCharacterControl.cs
@@ -5,23 +5,14 @@ using UnityEngine;
 public class CombatCharacterControl : MonoBehaviour {
 
     public float moveMaxSpeed;
-    public float rotateSpeed;
     public float moveForce;
 
-<<<<<<< HEAD
     private Rigidbody2D theRigidbody;
-=======
-    private Rigidbody2D thisRigidbody;
->>>>>>> e3d61b987f1b9c658987e5f7f70d7e5acbe5b08a
 
 	// Use this for initialization
 	void Start () 
     {
-<<<<<<< HEAD
         theRigidbody = GetComponent<Rigidbody2D>();
-=======
-        thisRigidbody = GetComponent<Rigidbody2D>();
->>>>>>> e3d61b987f1b9c658987e5f7f70d7e5acbe5b08a
 	}
 	
 	// Update is called once per frame
@@ -29,7 +20,6 @@ public class CombatCharacterControl : MonoBehaviour {
     {
         // move
         Vector2 force = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * moveForce;
-<<<<<<< HEAD
         theRigidbody.AddForce(force);
         if(theRigidbody.velocity.magnitude > moveMaxSpeed)
         {
@@ -44,19 +34,5 @@ public class CombatCharacterControl : MonoBehaviour {
             float targetAngle = Mathf.Atan2(mousePos.y - thisPos.y, mousePos.x - thisPos.x);
             this.transform.Rotate(0, 0, Mathf.Rad2Deg * targetAngle - this.transform.eulerAngles.z);
         }
-=======
-        thisRigidbody.AddForce(force);
-        if(thisRigidbody.velocity.magnitude > moveMaxSpeed)
-        {
-            thisRigidbody.velocity = thisRigidbody.velocity.normalized * moveMaxSpeed; 
-        }
-
-
-        // rotation
-        float rotateCoef = 0;
-        if (Input.GetKey(KeyCode.J)) rotateCoef += -1;
-        if (Input.GetKey(KeyCode.K)) rotateCoef += 1;
-        transform.Rotate(0, 0, rotateCoef * rotateSpeed * Time.deltaTime);
->>>>>>> e3d61b987f1b9c658987e5f7f70d7e5acbe5b08a
 	}
 }
